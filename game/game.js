82e13840cc4349a078b864a5a7198db4594def0d
Sarvesh Jain <sarvesh@ost.com>
2018-08-24 22:52:17 +0200

    Initial game commit

---
+++ game/game.js
@@ -0,0 +1,63 @@
+const Mosaic = require('index.js');
+
+const mosaicConfig = {
+  "origin": {
+    "provider": "ws://127.0.0.1:18545"
+  },
+  "auxiliaries": [
+    {
+      "provider": "ws://127.0.0.1:19546",
+      "originCoreContractAddress": "0x0000000000000000000000000000000000000001"
+    }
+  ]
+};
+
+function Game(originCoreAddress, originWorker, auxiliaryWorker) {
+  this.mosaic = new Mosaic('', mosaicConfig);
+  this.origin = this.mosaic.origin();
+  this.auxiliary = this.mosaic.core(originCoreAddress);
+  this.core = this.mosaic.contracts.core;
+  this.originBlockNumber = 0;
+  this.auxiliaryBlockNumber = 0;
+  this.originWorker = originWorker;
+  this.auxiliaryWorker = auxiliaryWorker;
+}
+
+Game.prototype = {
+
+  run: async function () {
+
+    await this.commitStateRoot();
+  },
+
+  commitStateRoot: async function () {
+
+    let originBlock = await this.origin.eth.getBlockNumber('latest');
+    let auxiliaryBlock = await this.auxiliary.eth.getBlockNumber('latest');
+
+    if (this.auxiliaryBlockNumber < auxiliaryBlock.blockNumber) {
+      // TODO Worker unlocking
+      let receipt = await this.core.origin
+        .commitStateRoot(auxiliaryBlock.blockNumber, auxiliaryBlock.stateRoot)
+        .send({from: this.originWorker});
+
+      console.log("auxiliary receipt", receipt);
+      this.auxiliaryBlockNumber = auxiliaryBlock.blockNumber;
+
+    }
+    if (this.originBlockNumber < originBlock.blockNumber) {
+      // TODO Worker unlocking
+      let receipt = await this.core.auxiliary
+        .commitStateRoot(originBlock.blockNumber, originBlock.stateRoot)
+        .send({from: this.originWorker});
+
+      console.log("origin receipt", receipt);
+
+      this.originBlockNumber = originBlock.blockNumber;
+    }
+    console.log("Waiting for new block......");
+    setTimeout(commitStateRoot, 10000);
+  }
+};
+
+new Game().run();
