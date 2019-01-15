7399e0bb2fc2707e09fd4d0e8463150f10b23853
Puneet Khushwani <puneet.khushwani@ost.com>
2019-01-15 14:37:26 +0530

    added methods to return raw tx object

--- libs/helpers/setup/OrganizationHelper.js
+++ libs/helpers/setup/OrganizationHelper.js
@@ -65,11 +65,39 @@ class OrganizationHelper {
   deploy(owner, admin, workers, expirationHeight, txOptions, web3) {
     const oThis = this;
     web3 = web3 || oThis.web3;
+    admin = admin || '0x0000000000000000000000000000000000000000';
+
+    let tx = oThis._deployRawTx(owner, admin, workers, expirationHeight, txOptions, web3);
+
+    console.log('* Deploying Organization Contract');
+
+    let txReceipt;
+    return tx
+      .send(txOptions)
+      .on('transactionHash', function(transactionHash) {
+        console.log('\t - transaction hash:', transactionHash);
+      })
+      .on('error', function(error) {
+        console.log('\t !! Error !!', error, '\n\t !! ERROR !!\n');
+        return Promise.reject(error);
+      })
+      .on('receipt', function(receipt) {
+        txReceipt = receipt;
+        console.log('\t - Receipt:\n\x1b[2m', JSON.stringify(receipt), '\x1b[0m\n');
+      })
+      .then(function(instace) {
+        oThis.address = instace.options.address;
+        console.log('\t - Organization Contract Address:', oThis.address);
+        return txReceipt;
+      });
+  }
+
+  _deployRawTx(owner, admin, workers, expirationHeight, txOptions, web3) {
+    const oThis = this;
+
     const abiBinProvider = oThis.abiBinProvider;
     const abi = abiBinProvider.getABI(ContractName);
     const bin = abiBinProvider.getBIN(ContractName);
-
-    admin = admin || '0x0000000000000000000000000000000000000000';
 
     let defaultOptions = {
       gas: '1600000'
@@ -94,34 +122,14 @@ class OrganizationHelper {
 
     const contract = new web3.eth.Contract(abi, null, txOptions);
     let args = [owner, admin, workers, _expirationHeight];
-    let tx = contract.deploy(
+
+    return contract.deploy(
       {
         data: bin,
         arguments: args
       },
       txOptions
     );
-
-    console.log('* Deploying Organization Contract');
-    let txReceipt;
-    return tx
-      .send(txOptions)
-      .on('transactionHash', function(transactionHash) {
-        console.log('\t - transaction hash:', transactionHash);
-      })
-      .on('error', function(error) {
-        console.log('\t !! Error !!', error, '\n\t !! ERROR !!\n');
-        return Promise.reject(error);
-      })
-      .on('receipt', function(receipt) {
-        txReceipt = receipt;
-        console.log('\t - Receipt:\n\x1b[2m', JSON.stringify(receipt), '\x1b[0m\n');
-      })
-      .then(function(instace) {
-        oThis.address = instace.options.address;
-        console.log('\t - Organization Contract Address:', oThis.address);
-        return txReceipt;
-      });
   }
 
   setAdmin(adminAddress, txOptions, contractAddress, web3) {
