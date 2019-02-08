444683e44d270969902957ab0f575af03710a940
Sarvesh Jain <sarvesh@ost.com>
2019-02-08 16:13:09 +0530

    Removed duplicate method from AssertAsyc

--- test_utils/AssertAsync.js
+++ test_utils/AssertAsync.js
@@ -9,21 +9,6 @@ class AssertAsync {
   static async reject(promise, message) {
     try {
       await promise;
-      throw new TypeError('Promise must reject');
-    } catch (exception) {
-      assert.strictEqual(
-        exception.message,
-        message,
-        `Exception reason must be "${message}" but found "${
-          exception.message
-        }"`,
-      );
-    }
-  }
-
-  static async reject(promise, message) {
-    try {
-      await promise;
       throw new Error('Promise must reject');
     } catch (exception) {
       assert.strictEqual(
